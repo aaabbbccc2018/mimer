dbeb7185559fab1729a98b937777b6826f6bc0d9
skybosi <skybosi_ok@163.com>
2017-08-11 12:34:40 +0800

    update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 #清除一些变量
 include $(CLEAR_VARS)
 #要生成的库名
-LOCAL_MODULE    := libmqtter
+LOCAL_MODULE    := libmimer
 #指定平台
 #LOCAL_ARM_MODE := arm
 #需要引用的库
@@ -13,15 +13,15 @@ LOCAL_CPPFLAGS += -fexceptions -frtti
 #编译参数
 LOCAL_CFLAGS := -Wall -O3 -enable-threads
 #定义宏
-TOP_INCLUDE := $(LOCAL_PATH)/../MQTTProtocol
+TOP_INCLUDE := $(LOCAL_PATH)/../MIMProtocol
 LOCAL_C_INCLUDES := $(TOP_INCLUDE)
 
 $(warning "include" $(TOP_INCLUDE) $(LOCAL_C_INCLUDES))
-TOP_SRC := $(LOCAL_PATH)/../MQTTProtocol
+TOP_SRC := $(LOCAL_PATH)/../MIMProtocol
 #库对应的源文件
-LOCAL_SRC_FILES := $(TOP_SRC)/MQTTInt.cpp \
-	$(TOP_SRC)/MQTTPacket.cpp             \
-	$(TOP_SRC)/MQTTProtocol.cpp           \
+LOCAL_SRC_FILES := $(TOP_SRC)/MIMInt.cpp \
+	$(TOP_SRC)/MIMPacket.cpp             \
+	$(TOP_SRC)/MIMProtocol.cpp           \
 	$(TOP_SRC)/Stream.cpp
 
 $(warning "src" $(LOCAL_SRC_FILES))
