28c62a413d32c498f010c9ea911d042963d0749e
skybosi <skybosi_fxc@163.com>
2017-10-24 23:32:07 +0800

    add default log format configure& add default color

--- ellog/ellog.cpp
+++ ellog/ellog.cpp
@@ -8,6 +8,27 @@ el::base::type::StoragePointer ellog::shared()
 	   return el::Helpers::storage();
 }
 
+bool ellog::config(const std::string& confile)
+{
+    if (_confile.empty())
+    {
+        _confile = confile;
+        _conf = el::Configurations(_confile);
+        el::Loggers::reconfigureAllLoggers(_conf);
+        return true;
+    }
+    return false;
+}
+
+void ellog::defaultformat(el::Configurations& idConf)
+{
+    idConf.set(el::Level::Trace,   el::ConfigurationType::Format, LOGGER_COLOR(BOLD,   "[ %level | %datetime ] > %msg"));
+    idConf.set(el::Level::Debug,   el::ConfigurationType::Format, LOGGER_COLOR(WHITE,  "[ %level | %datetime ] : %msg"));
+    idConf.set(el::Level::Error,   el::ConfigurationType::Format, LOGGER_COLOR(RED,    "[ %level | %datetime ] ! %msg"));
+    idConf.set(el::Level::Warning, el::ConfigurationType::Format, LOGGER_COLOR(YELLOW, "[ %level | %datetime ] ? %msg"));
+    idConf.set(el::Level::Info,    el::ConfigurationType::Format, LOGGER_COLOR(GREEN,  "[ %level | %datetime ] $ %msg"));
+}
+
 bool ellog::log(el::Level lev, const std::string& msg)
 {
     switch (lev) {
--- ellog/ellog.h
+++ ellog/ellog.h
@@ -9,32 +9,56 @@ namespace mim {
 
 class UTIL_API ellog
 {
+    // Formatting codes
+    #define RESET       "\033[00m"
+    #define BOLD        "\033[1m"
+    #define DARK        "\033[2m"
+    #define UNDERLINE   "\033[4m"
+    #define BLINK       "\033[5m"
+    #define REVERSE     "\033[7m"
+    #define CONCEALED   "\033[8m"
+    // Foreground colors
+    #define GREY        "\033[30m"
+    #define RED         "\033[31m"
+    #define GREEN       "\033[32m"
+    #define YELLOW      "\033[33m"
+    #define BLUE        "\033[34m"
+    #define MAGENTA     "\033[35m"
+    #define CYAN        "\033[36m"
+    #define WHITE       "\033[37m"
+    // Background colors
+    #define ON_GREY     "\033[40m"
+    #define ON_RED      "\033[41m"
+    #define ON_GREEN    "\033[42m"
+    #define ON_YELLOW   "\033[43m"
+    #define ON_BLUE     "\033[44m"
+    #define ON_MAGENTA  "\033[45m"
+    #define ON_CYAN     "\033[46m"
+    #define ON_WHITE    "\033[47m"
 public:
-    ellog(const std::string& id = "default", const std::string& confile = "") : _confile(confile), _id(id) {
+    ellog(const std::string& id = "default", const std::string& confile = "") : _confile(confile), _id(id)
+    {
         el::Helpers::setStorage(mim::ellog::shared());
+        _logger = el::Loggers::getLogger(id, true);
         if (!_confile.empty())
         {
             _conf = el::Configurations(_confile);
             el::Loggers::reconfigureAllLoggers(_conf);
         }
-        _logger = el::Loggers::getLogger(id, true);
+        else
+        {
+            el::Configurations idConf;
+            defaultformat(idConf);
+            el::Loggers::reconfigureLogger(id, idConf);
+            //_logger->configure(idConf);
+        }
     }
-    virtual ~ellog() {
-    }
+    virtual ~ellog() {}
 public:
     static el::base::type::StoragePointer shared();
     bool log(el::Level lev, const std::string& msg);
-    //el::Logger* operator->(){ return _logger; }
-public:
-    bool config(const std::string& confile){
-        if(_confile.empty()){
-            _confile = confile;
-            _conf = el::Configurations(_confile);
-            el::Loggers::reconfigureAllLoggers(_conf);
-            return true;
-        }
-        return false;
-    }
+    void defaultformat(el::Configurations& idConf);
+    bool config(const std::string& confile);
 public:
 #   define LOGGER_LEVEL_WRITERS_SIGNATURES(FUNCTION_NAME)\
     template <typename T, typename... Args>\
@@ -58,6 +82,9 @@ public:
 	LOGGER_LEVEL_WRITERS_SIGNATURES(fatal)
 	LOGGER_LEVEL_WRITERS_SIGNATURES(trace)
 #   undef LOGGER_LEVEL_WRITERS_SIGNATURES
+
+#define LOGGER_COLOR(COLOR, FORMAT) COLOR##FORMAT RESET
+
 private:
     el::Configurations  _conf;
     std::string         _id;
