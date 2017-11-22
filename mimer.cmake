8a4da9a0760e06932aa652571dc717bb4c4c1a30
skybosi <skybosi_ok@163.com>
2017-11-22 20:11:06 +0800

    update CMakeLists.txt fix build bug

--- mimer.cmake
+++ mimer.cmake
@@ -21,10 +21,18 @@ else( CMAKE_BUILD_TYPE STREQUAL "Debug" )
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O2 -Wno-sign-compare -pthread -fopenmp")
 endif( CMAKE_BUILD_TYPE STREQUAL "Debug" )
 
-message("Build Type:" ${CMAKE_BUILD_TYPE} ${CMAKE_CXX_FLAGS})
+#message("Build Type:" ${CMAKE_BUILD_TYPE} ${CMAKE_CXX_FLAGS})
 
 set(TOP_PATH ${CMAKE_MODULE_PATH})
-message("TOP path:" ${TOP_PATH})
+#message("TOP path:" ${TOP_PATH})
+
+if (NOT (PROJECT_NAME STREQUAL "Project"))
+	#set(BUILED_NAME ${PROJECT_NAME}_build)
+	set_property(GLOBAL PROPERTY ${PROJECT_NAME}_build TRUE)
+	get_property(BUILED_MODULE_GET GLOBAL PROPERTY "${PROJECT_NAME}_build")
+	message("PROJECT_NAME: ${PROJECT_NAME} is defined variable: ${BUILED_MODULE_GET}")
+endif(NOT (PROJECT_NAME STREQUAL "Project"))
+
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${TOP_PATH}/lib)
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${TOP_PATH}/lib)
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${TOP_PATH}/bin)
