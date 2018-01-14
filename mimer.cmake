10c32fe5f7d1a97c02e2664da839dc8c6c986007
skybosi <skybosi_fxc@163.com>
2018-01-14 13:04:37 +0800

    add android build && add libuv.cmake

--- mimer.cmake
+++ mimer.cmake
@@ -21,10 +21,18 @@ else( CMAKE_BUILD_TYPE STREQUAL "Debug" )
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O2 -Wno-sign-compare -fopenmp")
 endif( CMAKE_BUILD_TYPE STREQUAL "Debug" )
 
+
 #message("Build Type:" ${CMAKE_BUILD_TYPE} ${CMAKE_CXX_FLAGS})
 
 set(TOP_PATH ${CMAKE_MODULE_PATH})
 #message("TOP path:" ${TOP_PATH})
+
+if( CMAKE_PLATFORM_TYPE STREQUAL "android" )
+	add_definitions(-DCMAKE_TOOLCHAIN_FILE=$NDKROOT/build/cmake/android.toolchain.cmake-DANDROID_ARM_NEON=TRUE)
+	set(UVLIB_PATH ${CMAKE_MODULE_PATH}/lib/3rdlib/libuv/lib/android)
+else( CMAKE_PLATFORM_TYPE STREQUAL "android")
+	set(UVLIB_PATH ${CMAKE_MODULE_PATH}/lib/3rdlib/libuv/lib)
+endif( CMAKE_PLATFORM_TYPE STREQUAL "android" )
 
 if (NOT (PROJECT_NAME STREQUAL "Project"))
 	#set(BUILED_NAME ${PROJECT_NAME}_build)
